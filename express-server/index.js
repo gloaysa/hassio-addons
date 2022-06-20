973bc22d055dfcded539e3a0c7dc6d0d54177219
Guillermo Loaysa <gloaysa@gmail.com>
2022-06-20 21:38:57 +0200

    feat: add first route

--- express-server/index.js
+++ express-server/index.js
@@ -1,11 +1,37 @@
-const express = require('express')
+import express from 'express';
+import fetch from 'node-fetch';
 const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`Hello World!`)
 })
 
 app.listen(port, () => {
     console.log(`Express server listening on port ${port}`)
+})
+
+
+app.get('/gas-prices', async (req, res) => {
+    let responseCode = 0;
+    const {municipio} = req.query;
+    const { ListaEESSPrecio } = await fetch(`https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/${municipio}`)
+        .then((res) => {
+            responseCode = res.status;
+            return res;
+        })
+        .then((res) => res.json());
+
+    const [first, second, third] = ListaEESSPrecio?.filter(station => station['Precio Gasolina 95 E5'] !== '')
+        .map((station) => {
+            const latitud = station["Latitud"].replace(",",".");
+            const longitud = station["Longitud (WGS84)"].replace(",",".");
+            const mapa = "https://www.google.com/maps/dir/?api=1&destination=" + latitud + "," + longitud;
+            const marca = station["Rótulo"];
+            const precio = parseFloat(station['Precio Gasolina 95 E5'].replace(',', '.'));
+            return {latitud, longitud, mapa, marca, precio};
+        })
+        .sort((a, b) => a.precio - b.precio)
+
+    res.json({responseCode, first, second, third});
 })
