f483aed74c9e5dc4b26dacd7834ae1b1f26361d6
Guillermo Loaysa <gloaysa@gmail.com>
2022-06-20 20:56:28 +0200

    fix: build

--- express-server/run.sh
+++ express-server/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+if [ -n "$TZ" ] && [ -f /etc/localtime ]; then
+     if [ -f /usr/share/zoneinfo/"$TZ" ]; then
+         echo "Timezone set from $(cat /etc/timezone) to $TZ"
+         ln -snf /usr/share/zoneinfo/"$TZ" /etc/localtime && echo "$TZ" >/etc/timezone
+     fi
+ fi
 
 CONFIG_PATH=/data/options.json
 CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
