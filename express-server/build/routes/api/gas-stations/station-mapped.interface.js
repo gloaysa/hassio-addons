c861d298173b5053d86bd115e08afecfc477fb00
Guillermo Loaysa <gloaysa@gmail.com>
2023-02-10 23:55:45 +0100

    chore: remove build

--- express-server/build/routes/api/gas-stations/station-mapped.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=station-mapped.interface.js.map
--- express-server/build/services/carburante/carburante.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=carburante.interface.js.map
